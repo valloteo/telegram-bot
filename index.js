3e4d845bb5ec716ab02f7cb012bf210505bde79a
valloteo <vallo.teo@hotmail.it>
2026-02-12 16:54:49 +0100

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import { Telegraf } from "telegraf";
+import http from "http";
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
-bot.start((ctx) => {
-    ctx.reply("Bot attivo! 🚀");
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK - Bot is running");
+}).listen(process.env.PORT || 3000, () => {
+  console.log("Keep-alive server running");
 });
 
-bot.launch();
+bot.start((ctx) => {
+  ctx.reply("Bot attivo! 🚀\nPronto a ricevere comandi.");
+});
 
-console.log("Bot avviato!");
+bot.launch().then(() => {
+  console.log("Bot Telegram avviato correttamente!");
+});
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
